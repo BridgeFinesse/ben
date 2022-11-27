e2519e78ea75e0c1541f8d357c2bb7ae100d05dd
Jay Whipple <jay3@thecommongame.com>
2022-11-26 19:55:38 -0600

    Support client IP

--- src/gameserver.py
+++ src/gameserver.py
@@ -7,6 +7,9 @@ import websockets
 import game
 import human
 
+import random
+import TCGUtils
+
 from nn.models import Models
 
 
@@ -21,25 +24,37 @@ def worker(driver):
 
 
 async def handler(websocket, path):
-    print(f'got websocket connection {websocket}')
-
-    rdeal = game.random_deal()
+	print(f'got websockett connection {websocket} ')
+	client_ip = (websocket.remote_address)[0]   ##J3
+	print(f'got path {path}')
+  
+    #rdeal = game.random_deal()
+	if 'blah' in path:
+		rdeal = TCGUtils.getdeal(path,client_ip)
+	else:
+		rdeal = path.replace("/", "")
+		rdeal = rdeal.replace("%20", " ")
+		rdeal = rdeal.replace("%7C", "|")
+		rdeal = rdeal.split("|")[0:2] 
+		
     
-    driver = game.Driver(MODELS, human.WebsocketFactory(websocket))
-    driver.set_deal(*rdeal)
-    driver.human = [False, False, True, False]
+	driver = game.Driver(MODELS, human.WebsocketFactory(websocket))
+	driver.set_deal(*rdeal)
+	driver.human = [False, False, True, False]
 
-    try:
-        await driver.run()
 
-        with shelve.open('gamedb') as db:
-            deal_bots = driver.to_dict()
-            db[uuid.uuid4().hex] = deal_bots
-            print('saved')
+	try:
+		await driver.run()
+
+		with shelve.open('gamedb') as db:
+			deal_bots = driver.to_dict()
+			deal_bots['client_ip']= client_ip  ##J3
+			db[uuid.uuid4().hex] = deal_bots
+			print('saved')
     
-    except Exception as ex:
-        print('Error:', ex)
-        raise ex
+	except Exception as ex:
+		print('Error:', ex)
+		raise ex
 
 
 start_server = websockets.serve(handler, "0.0.0.0", 4443)
